d1fd7c49367a31e6d14c11e90bd1283b82571597
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-14 08:35:55 +0800

    tambah ()

--- php/Aplikasi/Fail/Papar/cari/template_biasa.php
+++ php/Aplikasi/Fail/Papar/cari/template_biasa.php
@@ -6,7 +6,7 @@ foreach ($this->senarai as $myTable => $row)
 	else
 	{
 		$tajukjadual = '<span class="badge badge-success">' . $myTable . '</span>'
-		. "\r" . '<span class="badge">' . count($row) . '</span>';
+		. "\r" . '<span class="badge">(' . count($row) . ')</span>';
 		echo "\n"; ?>
 <!-- Jadual <?php echo $myTable ?> ########################################### -->
 <?php include 'pilih_' . $pilihJadual . '.php'; ?>
